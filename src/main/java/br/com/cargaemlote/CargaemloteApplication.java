b2f304539968191c99f5d2c713c9639cd0de415f
V1centR <vicentcdb@gmail.com>
2022-01-09 17:26:30 -0500

    start project

---
+++ src/main/java/br/com/cargaemlote/CargaemloteApplication.java
@@ -0,0 +1,13 @@
+package br.com.cargaemlote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaemloteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaemloteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cargaemlote/CargaemloteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cargaemlote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaemloteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
